d9fbc49143cb2ddf01da35154c4cc53c8eec7fb3
ltw <tong-min@live.cn>
2017-08-17 13:41:45 +0800

    git add all

---
+++ app/src/main/java/com/example/tong/wechat/Index.java
@@ -0,0 +1,17 @@
+package com.example.tong.wechat;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tong on 2016/10/17.
+ */
+
+public class Index extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.index);
+    }
+}
